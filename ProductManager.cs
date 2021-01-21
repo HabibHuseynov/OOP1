3c45f0ac88f1d899976d721a728d6889f393f80a
Habib <hbbhsnv@gmail.com>
2021-01-21 21:55:19 +0400

    Add project files.

---
+++ ProductManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " Add");
+
+        }
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " Update");
+        }
+    }
+}
